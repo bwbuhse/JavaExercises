e88f4be8ca80f8b98779a4c8fc25e8284c5785a0
bwbuhse <bwbuhse@gmail.com>
2017-03-22 13:47:15 -0500

    Shortened RollercoasterWords using a File and Stream

--- src/misc/RollercoasterWords.java
+++ src/misc/RollercoasterWords.java
@@ -1,46 +1,37 @@
 package misc;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class RollercoasterWords {
-	public static void main(String[] args) throws FileNotFoundException {
-		Scanner file = new Scanner(new File("src/enable1.txt"));
+	public static void main(String[] args) throws IOException {
+		long start = System.currentTimeMillis();
+		System.out.println(
+				Files.lines(Paths.get("src/enable1.txt"))
+						.filter(s -> s.length() > 4)
+						.filter(s -> {
+							boolean direction;
+							if (s.charAt(0) < s.charAt(1))
+								direction = false;
+							else if (s.charAt(0) > s.charAt(1))
+								direction = true;
+							else
+								return false;
 
-		HashSet<String> words = new HashSet<>();
-		while (file.hasNext()) {
-			String word = file.nextLine();
-			if (word.length() > 4 && isRollercoaster(word))
-				words.add(word);
-		}
-		System.out.println(words.size());
-
+							for (int i = 1; i < s.length() - 1; i++) {
+								char at = s.charAt(i);
+								char next = s.charAt(i + 1);
+								if (direction && at >= next)
+									return false;
+								else if (!direction && at <= next)
+									return false;
+								direction = !direction;
+							}
+							return true;
+						})
+						.count()
+		);
+		System.out.println(System.currentTimeMillis() - start);
 	}
-
-	private static boolean isRollercoaster(String word) {
-		boolean direction;
-		if (word.charAt(0) < word.charAt(1))
-			direction = false;
-		else if (word.charAt(0) > word.charAt(1))
-			direction = true;
-		else
-			return false;
-
-		for (int i = 1; i < word.length() - 1; i++) {
-			char at = word.charAt(i);
-			char next = word.charAt(i + 1);
-			if (direction) {
-				if (at >= next)
-					return false;
-			} else {
-				if (at <= next)
-					return false;
-			}
-			direction = !direction;
-		}
-		return true;
-	}
-
 }
