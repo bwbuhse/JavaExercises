092573f1e082cb8465ca6f6176dc3d75513d0fe1
bwbuhse <bwbuhse@gmail.com>
2017-04-12 11:38:38 -0500

    Completed on Honza in src/compproblems/region2017

--- src/compproblems/region2017/Honza.java
+++ src/compproblems/region2017/Honza.java
@@ -1,21 +1,20 @@
 package compproblems.region2017;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
-import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class Honza {
-	public static void main(String[] args) throws FileNotFoundException {
-		Scanner file = new Scanner(new File("src/compproblems/region2017/honza.dat"));
-		int sets = file.nextInt();
-		file.nextLine();
-		for (int s = 0; s < sets; s++) {
-			int[] nums = Stream.of(file.nextLine().split("[/ :]")).mapToInt(Integer::parseInt).toArray();
+	public static void main(String[] args) throws IOException {
+		Files.lines(Paths.get("src/compproblems/region2017/honza.dat")).forEach(s -> {
+			if (s.length() == 0 || !s.contains(" "))
+				return;
+			int[] nums = Stream.of(s.split("[/ :]")).mapToInt(Integer::parseInt).toArray();
 			LocalDateTime time = LocalDateTime.of(nums[2], nums[0], nums[1], nums[3], nums[4]).plusMinutes(nums[5] - 5);
 			System.out.printf("%s %d/%d/%d %02d:%02d%n", time.getDayOfWeek(), time.getMonthValue()
 					, time.getDayOfMonth(), time.getYear(), time.getHour(), time.getMinute());
-		}
+		});
 	}
 }
