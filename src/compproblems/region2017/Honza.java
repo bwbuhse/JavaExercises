b44e1f67d7308ca9c513fd873160157b73fd516a
bwbuhse <bwbuhse@gmail.com>
2017-04-12 14:26:15 -0500

    Improved Honza in src/compproblems/region2017

--- src/compproblems/region2017/Honza.java
+++ src/compproblems/region2017/Honza.java
@@ -8,13 +8,12 @@ import java.util.stream.Stream;
 
 public class Honza {
 	public static void main(String[] args) throws IOException {
-		Files.lines(Paths.get("src/compproblems/region2017/honza.dat")).forEach(s -> {
-			if (s.length() == 0 || !s.contains(" "))
-				return;
-			int[] nums = Stream.of(s.split("[/ :]")).mapToInt(Integer::parseInt).toArray();
-			LocalDateTime time = LocalDateTime.of(nums[2], nums[0], nums[1], nums[3], nums[4]).plusMinutes(nums[5] - 5);
-			System.out.printf("%s %d/%d/%d %02d:%02d%n", time.getDayOfWeek(), time.getMonthValue()
-					, time.getDayOfMonth(), time.getYear(), time.getHour(), time.getMinute());
-		});
+		Files.lines(Paths.get("src/compproblems/region2017/honza.dat"))
+				.filter(s -> s.length() > 0 && s.contains(" "))
+				.map(s -> Stream.of(s.split("[/ :]")).mapToInt(Integer::parseInt).toArray())
+				.map(nums -> LocalDateTime.of(nums[2], nums[0], nums[1], nums[3], nums[4]).plusMinutes(nums[5] - 5))
+				.forEach(time -> System.out.printf("%s %d/%d/%d %02d:%02d%n", time.getDayOfWeek(), time.getMonthValue()
+						, time.getDayOfMonth(), time.getYear(), time.getHour(), time.getMinute())
+				);
 	}
 }
