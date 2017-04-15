cac056c0d524298d5122452e5061ab13a1e67758
bwbuhse <bwbuhse@gmail.com>
2017-04-15 12:07:08 -0500

    Worked on Problem18 in src/misc/euler

--- src/misc/euler/Problem18.java
+++ src/misc/euler/Problem18.java
@@ -22,26 +22,33 @@ public class Problem18 {
 			triangle[i] = Stream.of(file.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 		}
 
-		open.add(new Node(triangle[0][0], triangle[0][0], triangle[1][0], triangle[1][1]));
+		open.add(new Node(0, 0, triangle[0][0], triangle[1][0], triangle[1][1]));
 		while (!open.isEmpty() && open.get(0).leftChild >= 0) {
 			Node current = open.remove(0);
 			closed.add(current);
-
+			solve(current);
+			Collections.sort(open);
 		}
 	}
 
+	// TODO how?
+	private static void solve(Node node) {
+	}
+
 	static class Node implements Comparable<Node> {
-		int num;
+		int r, c;
 		int sum;
 		int leftChild;
 		int rightChild;
 
-		Node(int num) {
-			this.num = num;
+		public Node(int r, int c) {
+			this.r = r;
+			this.c = c;
 		}
 
-		Node(int num, int sum, int leftChild, int rightChild) {
-			this.num = num;
+		public Node(int r, int c, int sum, int leftChild, int rightChild) {
+			this.r = r;
+			this.c = c;
 			this.sum = sum;
 			this.leftChild = leftChild;
 			this.rightChild = rightChild;
@@ -54,20 +61,20 @@ public class Problem18 {
 
 			Node node = (Node) o;
 
-			return num == node.num && leftChild == node.leftChild && rightChild == node.rightChild;
+			if (r != node.r) return false;
+			return c == node.c;
 		}
 
 		@Override
 		public int hashCode() {
-			int result = num;
-			result = 31 * result + leftChild;
-			result = 31 * result + rightChild;
+			int result = r;
+			result = 31 * result + c;
 			return result;
 		}
 
 		@Override
 		public int compareTo(@NotNull Node o) {
-			return num - o.num;
+			return o.sum - sum;
 		}
 	}
 }
