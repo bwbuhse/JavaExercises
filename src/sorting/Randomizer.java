ee9804665dba9ca83b4b18e5712ddf15245acf8f
bwbuhse <bwbuhse@gmail.com>
2017-04-14 22:03:53 -0500

    Created Randomizer.intList() with range in src/sorting

--- src/sorting/Randomizer.java
+++ src/sorting/Randomizer.java
@@ -6,9 +6,13 @@ class Randomizer {
 	}
 
 	static int[] intList(int n) {
+		return intList(n, 1, 50);
+	}
+
+	static int[] intList(int n, int min, int max) {
 		int[] list = new int[n];
 		for (int i = 0; i < list.length; i++) {
-			list[i] = (int) (Math.random() * 50) + 1;
+			list[i] = (int) (Math.random() * (max - min)) + min;
 		}
 		return list;
 	}
